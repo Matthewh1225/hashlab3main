5115021b646606c30ea45a800ed43e4a5b848136
Matthewh1225 <mhuckman97@yahoo.com>
2025-11-21 13:03:05 -0600

    hashtable

--- src/Assignment_Output.adb
+++ src/Assignment_Output.adb
@@ -1,7 +1,7 @@
 
-with Ada.Text_IO;                 use Ada.Text_IO;
-with Ada.Integer_Text_IO;         use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO;           use Ada.Float_Text_IO;
+with Ada.Text_IO;   use Ada.Text_IO;
+with Ada.Integer_Text_IO;    use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;  use Ada.Float_Text_IO;
 with Ada.Numerics.Elementary_Functions;  use Ada.Numerics.Elementary_Functions;
 with Hash_Type;   use Hash_Type;
 
